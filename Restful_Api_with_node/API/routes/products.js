4da8351743cf846e578dbbb870371271e91d66ec
Esseh12 <gbenmoese788@gmail.com>
2024-05-23 19:00:32 +0100

    adding routes

---
+++ Restful_Api_with_node/API/routes/products.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET requests to /products'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling POST requests to /products'
+    });
+});
+
+//about a single product
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if (id === 'special') { 
+        res.status(200).json({
+            message: 'you discovered the special Id',
+            id: id
+        });
+    } else {
+    res.status(200).json({
+        message: 'you passed an Id'
+    });
+    }
+});
+
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated product'
+    });
+});
+
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Delected product!'
+    });
+});
+
+module.exports = router;
